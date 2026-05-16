bab8dd8f6f049477392013b38f189281c32bca2c
Neemias Cormino Manso <130704790+n33miaz@users.noreply.github.com>
2026-05-15 21:24:11 -0300

    feat: rabbitmq async events, resilience config, testcontainers deps, docker compose

---
+++ src/main/java/br/com/painel_economico/service/event/rabbit/CategorizationConsumer.java
@@ -0,0 +1,19 @@
+package br.com.painel_economico.service.event.rabbit;
+
+import br.com.painel_economico.service.event.StatementImportedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Profile("rabbit")
+@Component
+public class CategorizationConsumer {
+
+    @RabbitListener(queues = RabbitConfig.CATEGORIZE_QUEUE)
+    public void onStatementImported(StatementImportedEvent event) {
+        log.info("[async-categorize] received statement.imported user={} count={}",
+                event.getUserId(), event.getTransactionsImported());
+    }
+}
